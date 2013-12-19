a3dbd94dc19657f0e59c4a646431693e29df09d6
saasbook <saasbook@saasbook.(none)>
2013-12-18 23:57:42 -0800

    change views

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ DoSomthIn::Application.routes.draw do
 
   resources :users
 
+  resources :comments
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131219014513) do
+ActiveRecord::Schema.define(:version => 20131219051949) do
+
+  create_table "comments", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "comment_id"
+    t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "issues", :force => true do |t|
     t.string   "title"
