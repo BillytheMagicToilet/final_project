24a2d58fcc13b0b7f26f4160735727da0d92fa89
Spencer Comerford <Spencevail@gmail.com>
2013-12-19 02:26:45 -0700

    Fixed migration overlapping.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131219051949) do
+ActiveRecord::Schema.define(:version => 20131219085123) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
@@ -25,9 +25,10 @@ ActiveRecord::Schema.define(:version => 20131219051949) do
     t.string   "title"
     t.text     "description"
     t.text     "goal"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
     t.integer  "user_id"
+    t.integer  "votes_count", :default => 0
   end
 
   create_table "users", :force => true do |t|
