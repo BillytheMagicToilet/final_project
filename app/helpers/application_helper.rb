6f53a32bf0985a4f15ff80d8df180889bda781ac
Spencer Comerford <Spencevail@gmail.com>
2013-12-18 18:56:36 -0700

    Initial Devise setup.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def alert_class_type(name)
+
+    # binding.pry
+
+    danger_names = ['alert', :alert]
+
+    if danger_names.include?(name)
+      return 'alert-danger'
+    else
+      return 'alert-warning'
+    end
+
+  end
+  
 end
